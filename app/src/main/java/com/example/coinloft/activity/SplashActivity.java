4ef0b813867168096c38f84345f9b4cc96200f10
Майк <michael.fomin.89@gmail.com>
2019-08-10 15:28:59 +0300

    import improvements SplashActivity MainActivity with a delay of 2 seconds

--- app/src/main/java/com/example/coinloft/activity/SplashActivity.java
+++ app/src/main/java/com/example/coinloft/activity/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.coinloft.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -7,11 +8,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         new Handler().postDelayed(()->{
-
+            final Intent intent = new Intent( this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
         }, 2000);
     }
 }
