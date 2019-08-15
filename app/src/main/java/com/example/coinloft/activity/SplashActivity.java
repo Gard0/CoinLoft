2911a5f179e04fe94ef066ef56e4c94c46d0c578
Майк <michael.fomin.89@gmail.com>
2019-08-15 22:15:02 +0300

    WelcomeActivity screen created. Manifest improvements.
    else
    Code readability improvements.
    Import optimized.

--- app/src/main/java/com/example/coinloft/activity/SplashActivity.java
+++ app/src/main/java/com/example/coinloft/activity/SplashActivity.java
@@ -1,11 +1,15 @@
 package com.example.coinloft.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coinloft.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,10 +19,17 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
         new Handler().postDelayed(() -> {
-            final Intent intent = new Intent(this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+            if (prefs.getBoolean("show_welcome_screen", true)) {
+                startActivity(new Intent(this, WelcomeActivity.class));
+            } else {
+                startActivity(new Intent(this, MainActivity.class));
+            }
+
         }, DELAY_START_ACTIVITY);
     }
 }
