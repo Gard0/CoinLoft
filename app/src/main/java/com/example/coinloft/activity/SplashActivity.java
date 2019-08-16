8edc7eba0aa3b1d911fc2ddfe2f4588557fd6475
Майк <michael.fomin.89@gmail.com>
2019-08-17 01:41:26 +0300

    Images added.
    Added link to button.
    
    Welcome screen view added (three tabs).
    
    Localization improvements. (ENG/RU)
    
    Screen view is close to design.
    
    When you restart it, you enter to the MainActivity. (The flag is written to the SharedPreferences application)
    
    else
    Code readability improvements.
    Import optimized.
    Gradle build update.

--- app/src/main/java/com/example/coinloft/activity/SplashActivity.java
+++ app/src/main/java/com/example/coinloft/activity/SplashActivity.java
@@ -29,7 +29,6 @@ public class SplashActivity extends AppCompatActivity {
             } else {
                 startActivity(new Intent(this, MainActivity.class));
             }
-
         }, DELAY_START_ACTIVITY);
     }
 }
--- app/src/main/java/com/example/coinloft/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/coinloft/activity/WelcomeActivity.java
@@ -1,16 +1,40 @@
 package com.example.coinloft.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-import com.example.coinloft.R;
+import com.example.coinloft.adapter.WelcomePagerAdapter;
+
+import static com.example.coinloft.R.id;
+import static com.example.coinloft.R.layout;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private TextView mButtonStart;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(layout.activity_welcome);
+        mViewPager = findViewById(id.pager);
+        mViewPager.setAdapter((new WelcomePagerAdapter(getLayoutInflater())));
+        mButtonStart = findViewById((id.button_start));
+        mButtonStart.setOnClickListener(View -> {
+            final SharedPreferences prefs = PreferenceManager
+                    .getDefaultSharedPreferences(this);
+            prefs.edit().putBoolean("show_welcome_screen", false).apply();
+            final Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+
     }
 }
