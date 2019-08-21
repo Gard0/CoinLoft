ff5611c61e46c284a12f6f7ede03a75995d1218a
Майк <michael.fomin.89@gmail.com>
2019-08-22 00:20:24 +0300

    Code readability improvements.

--- app/src/main/java/com/example/coinloft/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/coinloft/activity/WelcomeActivity.java
@@ -15,16 +15,14 @@ import static com.example.coinloft.R.id;
 import static com.example.coinloft.R.layout;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private ViewPager mViewPager;
-    private TextView mButtonStart;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_welcome);
-        mViewPager = findViewById(id.pager);
+        ViewPager mViewPager = findViewById(id.pager);
         mViewPager.setAdapter((new WelcomePagerAdapter(getLayoutInflater())));
-        mButtonStart = findViewById((id.button_start));
+        TextView mButtonStart = findViewById((id.button_start));
         mButtonStart.setOnClickListener(View -> {
             final Settings settings = Settings.of(this);
             settings.doNotShowWelcomeScreenNextTime();
